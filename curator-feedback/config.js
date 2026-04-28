941718a7c453736b48c661091d585c888d7003d8
Vladyslava <97236783+V1adyslava@users.noreply.github.com>
2026-04-28 23:03:11 +0300

    config.js

--- curator-feedback/config.js
+++ curator-feedback/config.js
@@ -2,5 +2,5 @@
 // (see README.md → "Setup"). Until you do, submissions will fail.
 window.CONFIG = {
   WEB_APP_URL:
-    "https://script.google.com/macros/s/AKfycbzV37SogU02UsUaUKR8jIJHNOgUG_ScqeWdmne8Y2wDcrXHjp3cJ2tEQuBSNxuAYBM/exec",
+    "https://script.google.com/macros/s/AKfycbws9WBzJ5lfnKZMNaVqm4flu7HaPEF6raLhJDgUgwv6xglKZ_cAWV0xUm6LyanQ__odJw/exec",
 };
